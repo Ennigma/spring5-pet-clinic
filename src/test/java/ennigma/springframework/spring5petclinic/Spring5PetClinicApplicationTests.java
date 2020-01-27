c4b0c9cccc5f798ab005eef8bc9f8f72a7e59998
Eugeniu Serbaniuc <eserbaniuc@costengineering.eu>
2020-01-27 15:59:31 +0200

    Initial Project Add

---
+++ src/test/java/ennigma/springframework/spring5petclinic/Spring5PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ennigma.springframework.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
